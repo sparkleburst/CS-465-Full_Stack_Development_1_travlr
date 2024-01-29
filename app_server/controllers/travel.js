b5b253730289acc867aca55ab74a9b1c970eec4f
maxgilhespy <maxine.gilhespy@snhu.edu>
2024-01-28 20:51:52 -0600

    Travel page is dynamic using JSON.

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,9 +1,14 @@
 console.log('Inside app_server, controllers, travel.js file.');
+
+var fs = require('fs');
+
+var trips = JSON.parse(fs.readFileSync('./data/trips.json', "utf8"));
+
 /* GET travel view */
 const travel = (req, res) => {
     try {
         console.log('Inside app_server, controllers, travel.js, travel function.');
-        res.render('travel', { title: 'Travlr Getaways '});
+        res.render('travel', { title: 'Travlr Getaways', trips});
     } catch (error) {
         console.error('Error in travel function:', error);
         res.status(500).send('Internal Sever Error');
