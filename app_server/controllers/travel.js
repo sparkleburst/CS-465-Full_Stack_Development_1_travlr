49783ffa876e0084cc99560a1f72d71980e05f5d
maxgilhespy <maxine.gilhespy@snhu.edu>
2024-02-13 08:50:17 -0600

    Added Angular but have problems with it running

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,10 +1,16 @@
 console.log('Inside app_server, controllers, travel.js file.');
 
-var fs = require('fs');
+const request = require('request');
+const apiOptions = {
+    server: 'http://localhost:3000'
+};
 
-var trips = JSON.parse(fs.readFileSync('./data/trips.json', "utf8"));
+// above allows API endpoints to pull data from database
+// below allowed pulling data from json files
+// var fs = require('fs');
+// var trips = JSON.parse(fs.readFileSync('./data/trips.json', "utf8"));
 
-/* GET travel view */
+/* GET travel view - This was the old way with json
 const travel = (req, res) => {
     try {
         console.log('Inside app_server, controllers, travel.js, travel function.');
@@ -15,7 +21,51 @@ const travel = (req, res) => {
         res.status(500).send('Internal Sever Error');
     }
 };
+*/
+
+/* render travel list view */
+const renderTravelList = (req, res, responseBody) => {
+    let message = null;
+    let pageTitle = process.env.npm_package_description + ' - Travel';
+
+    if (!(responseBody instanceof Array)) {
+        message = 'API lookup error';
+        responseBody = [];
+    } else {
+        if (!responseBody.length) {
+            message = 'Mo trips exist in database!';
+        }
+    }
+
+    res.render('travel', {
+        title: pageTitle,
+        trips: responseBody,
+        message
+    });
+};
+
+/* GET travel list view */
+const travelList = (req, res) => {
+    const path = '/api/trips';
+    const requestOptions = {
+        url: `${apiOptions.server}${path}`,
+        method: 'GET',
+        json: {},
+    };
+
+    console.info('>> travelController.travelList calling ' + requestOptions.url);
+
+    request(
+        requestOptions,
+        (err, { statusCode }, body) => {
+            if (err) {
+                console.error(err);
+            }
+            renderTravelList(req, res, body);
+        }
+    )
+}
 
 module.exports = {
-    travel
+    travelList
 };
--- app_server/routes/travel.js
+++ app_server/routes/travel.js
@@ -5,6 +5,6 @@ const controller = require('../controllers/travel');
 /* GET travel view. */
 console.log('Inside app_server, routes, travel.js.');
 // Define the route
-router.get('/', controller.travel);
+router.get('/', controller.travelList);
 
 module.exports = router;
